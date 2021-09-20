60382f0c56fcef95868e0182e4d062a809219250
Gyenese12 <gyenese94@gmail.com>
2021-09-20 09:09:57 +0200

    hf

---
+++ hf2_2.py
@@ -0,0 +1,4 @@
+koordinata1 = 3
+koordinata2 = 5
+if(koordinata1 < koordinata2): print("Távolság: ", koordinata2-koordinata1)
+if(koordinata1 > koordinata2): print("Távolság: ", koordinata1-koordinata2)
