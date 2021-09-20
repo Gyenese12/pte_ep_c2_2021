32301623a6d1535c7f417409c2d5e11919132abb
Gyenese12 <gyenese94@gmail.com>
2021-09-20 09:10:49 +0200

    hf

---
+++ hf2_3.py
@@ -0,0 +1,4 @@
+str_tomb = ["almaszár" , "kerékgyártó" , "Flóra-pihenő" , "malomvölgy" , "Misina" , "Égercőlgyi tó" , "Tenkes" , "Zsolnay-tó"]
+for x in str_tomb:
+            if(len(x) > 10) : print("Szó:",x,"Hossz: " , len(x) , "hosszabb mint 10")
+            else: print("Szó: " , x ," hossz: " , len(x))
