126f4a0a82ecc1dd728bf2e126fa0b7ebbd7acf6
Gyenese12 <gyenese94@gmail.com>
2021-09-20 08:28:21 +0200

    hf

---
+++ String_exercise.py
@@ -0,0 +1,12 @@
+py_string = "python"
+first_str = py_string[0]
+last_str = py_string[5]
+print("első:" , first_str , "utolsó: " , last_str)
+
+#-----------------------------------------------------------------------
+py_string = "Hiába a hegynyi anyag, a hallgató gyorsan halad.” mondatból"
+hallgato_str = py_string[25:33]
+print(hallgato_str)
+
+#-----------------------------------------------------------------------
+
